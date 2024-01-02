f4d83a742a166eaf18bc4b282d349c8fb4f6a19d
Gomez-Luis-Alfredo <alvisael@gmail.com>
2024-01-02 11:24:19 -0300

    Primer cambio

---
+++ index.js
@@ -0,0 +1,21 @@
+class personaje{
+    constructor(nombrePersonaje, idadePersonaje, tipoPersonaje, ataquePersonaje){
+        this.nombrePersonaje = nombrePersonaje
+        this.idadePersonaje = idadePersonaje
+        this.tipoPersonaje = tipoPersonaje
+        this.ataquePersonaje = ataquePersonaje
+    }
+    atacar(){
+        console.log(`O ${this.tipoPersonaje} atacou usando ${this.ataquePersonaje}`)
+    }
+}
+
+let guerreiroPersonaje = new personaje("Luis", "16", "Guerreiro", "Espada")
+let magoPersonaje = new personaje("Angel", "28", "Mago", "Magia")
+let mongePersonaje = new personaje("Gabriel", "20", "Monge", "Marciais")
+let ninjaPersonaje = new personaje("JUan", "30", "Ninja", "shuriken")
+
+guerreiroPersonaje.atacar();
+magoPersonaje .atacar();
+mongePersonaje.atacar();
+ninjaPersonaje.atacar();
